602a46240c913ad7ac739065b48b4005b39869ff
Srinivasu <mandavas@gmail.com>
2026-02-17 16:55:33 -0500

    Commit 3

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,8 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass-Stream2Learn-123");
         //Checking from Stream2 Learn
-        //Adding more chnages after merge
+        //Adding more changes after merge
+        //Changing 123456
+        System.out.println("DevClass-Stream2Learn-789");
     }
 }
